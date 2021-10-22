7efa100be5ce3f43c7a8e218efd31a522c5740dc
Pavel Che <alien.killer3@bk.ru>
2021-10-22 13:22:06 +0300

    connected bootstrap and laid out the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
+import React from "react";
+
+import { HeaderBlock } from "./components";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <HeaderBlock
+        title="Заголовок сайта"
+        description="Описание"
+        imageUrl="https://www.atlasandboots.com/wp-content/uploads/2019/05/ama-dablam2-most-beautiful-mountains-in-the-world.jpg"
+      />
+      <div className="container">
+        <button type="button" class="btn btn-primary">Primary</button>
+        <div className="content"></div>
+      </div>
     </div>
   );
 }
