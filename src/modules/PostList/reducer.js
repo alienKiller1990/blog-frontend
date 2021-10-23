e1857981561424ff7fa9b4b4f06f693c0da9f6c3
Pavel Che <alien.killer3@bk.ru>
2021-10-23 16:51:00 +0300

    created store, connected reduxDevtools, created posts.json

---
+++ src/modules/PostList/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    items: null,
+}
+
+export default (state = initialState, action) => {
+    const {type, payload} = action;
+    switch (type) {
+
+        default:
+            return state;
+    }
+}
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from './reducers'
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = composeEnhancer(
+    applyMiddleware(thunk)
+);
+
+const store = createStore(rootReducer, middleware);
+
+export default store;
